0f177c22b52daf08ffe0052cd949436bb3ff7219
solobaba <solobaba_odu@yahoo.com>
2018-04-10 09:55:08 +0100

    Second commit

--- app/src/main/java/com/example/mighty/mightydataairtelbackend/MainActivity.java
+++ app/src/main/java/com/example/mighty/mightydataairtelbackend/MainActivity.java
@@ -1,31 +1,44 @@
 package com.example.mighty.mightydataairtelbackend;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    Button mButton;
+    String code = "123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton = (Button)findViewById(R.id.check_balance);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Check_Balance(code);
+            }
+        });
+
     }
 
     // Get airtel balance(*123# for airtel)
-
-    public void dialNumber(String code) {
+    private void Check_Balance(String code) {
         String ussdCode = "*" + code + Uri.encode("#");
         startActivity(new Intent("android.intent.action.CALL", Uri.parse("tel:" + ussdCode)));
         startService(new Intent(this, USSDService.class));
-        dailNumber("*123#");
     }
 
-    private void dailNumber(String s) {
-    }
+//    public void dialNumber(View view) {
+//        dailNumber("*123#");
+//          startService(new Intent(this, USSDService.class));
+//    }
 
 }
--- app/src/main/java/com/example/mighty/mightydataairtelbackend/USSDService.java
+++ app/src/main/java/com/example/mighty/mightydataairtelbackend/USSDService.java
@@ -5,27 +5,63 @@ import android.accessibilityservice.AccessibilityServiceInfo;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.Collections;
+import java.util.List;
 
 public class USSDService extends AccessibilityService {
-    public static String TAG = "USSDService";
+
+    public static String TAG = USSDService.class.getSimpleName();
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         Log.d(TAG, "onAccessibilityEvent");
-        String text = event.getText().toString();
 
-        if (event.getClassName().equals("android.app.AlertDialog")) {
-            performGlobalAction(GLOBAL_ACTION_BACK);
-            Log.d(TAG, text);
-            Intent intent = new Intent("com.times.ussd.action.REFRESH");
-            intent.putExtra("message", text);
+        AccessibilityNodeInfo source = event.getSource();
+        /* if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED && !event.getClassName().equals("android.app.AlertDialog")) { // android.app.AlertDialog is the standard but not for all phones  */
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED && !String.valueOf(event.getClassName()).contains("AlertDialog")) {
+            return;
+        }
+        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED && (source == null || !source.getClassName().equals("android.widget.TextView"))) {
+            return;
+        }
+        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED && TextUtils.isEmpty(source.getText())) {
+            return;
         }
 
-        // write a broad cast receiver and call sendbroadcast() from here,
-        // if you want to parse the message for balance, date
+        List<CharSequence> eventText;
+
+        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            eventText = event.getText();
+        } else {
+            eventText = Collections.singletonList(source.getText());
+        }
+
+        String text = processUSSDText(eventText);
+
+        if( TextUtils.isEmpty(text) ) return;
+
+        // Close dialog
+        performGlobalAction(GLOBAL_ACTION_BACK); // This works on 4.1+ only
+
+        Log.d(TAG, text);
+        // Handle USSD response here
+    }
+
+    private String processUSSDText(List<CharSequence> eventText) {
+        for (CharSequence s : eventText) {
+            String text = String.valueOf(s);
+            // Return text if text is the expected ussd response
+            if( true ) {
+                return text;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -39,10 +75,8 @@ public class USSDService extends AccessibilityService {
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.flags = AccessibilityServiceInfo.DEFAULT;
         info.packageNames = new String[]{"com.android.phone"};
-        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED | AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
         setServiceInfo(info);
     }
-
-
 }
